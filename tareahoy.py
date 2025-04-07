29fe302d4ffe2a3310ceb54062ae982af8ea2179
Vicentep12 <vice.perezg@duocuc.cl>
2025-04-07 11:08:08 -0400

    Add files via upload

---
+++ tareahoy.py
@@ -0,0 +1,27 @@
+print("Hola")
+print("Mundo")
+
+#Comentarios adhoc
+"""Hola
+people!!
+Como se encuentran!!!!"""
+
+"""Variable01 = 12
+Variable02 = 12.3
+Variable03 = "Hola gente"
+Variable04 = True"""
+
+"""nombre = input ("Digame su nombre")
+print("Welcome to the jungle,"+ nombre)"""
+
+nombre = input ("Dime tu nombre")
+apellido = input ("Dime tu apellido")
+edad = input ("Dime tu edad")
+
+print("Hola", nombre)
+print("Tu apellido es", apellido)
+print("Tu edad es ", edad)
+
+
+
+ 
