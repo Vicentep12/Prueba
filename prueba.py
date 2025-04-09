2d16c4b6314b623f41993c94190a9a9cb0d07830
Vicentep12 <vice.perezg@duocuc.cl>
2025-04-09 09:35:15 -0400

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,24 @@
+edad =int(input("ingrese su edad: "))
+print("Hola, tu edad es " *3, edad)
+
+variable = 10
+variablefloat = 3.4
+variableSTr = "hola"
+variablebool = True
+
+n1 = int(input("Por favor ingrese su primero numero: "))
+n2 = int(input("Por favor ingrese su segundo numero: "))
+R1 = n1 * n2
+print("Tu multiplicacion es: ", R1)
+
+n3 = int(input("Ingresa un numero para dividir: "))
+n4 = int(input("Ingresa el otro numero: "))
+R2 = n3/n4 
+print("Tu resultado es", R2 )
+
+n5 = float(input("Ingresa tu numero decimal: "))
+n6 = int(input("Ingresa tu numero entero: "))
+n7 = int(input("Ingresa tu nuemero NEGATIVO: "))
+
+R3 = n5*n6/n7
+print("Tu resultado es", R3)
