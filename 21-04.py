00891cd6c910afc7b02e4848dbe3faa1b8e84289
Vicentep12 <vice.perezg@duocuc.cl>
2025-04-22 16:25:50 -0400

    Update 21-04.py

--- 21-04.py
+++ 21-04.py
@@ -1,11 +1,12 @@
 print("Bienvenido a Certifika!")
-nota = input(print("Por favor, ingresa tu califacion para ver a qué descuento accedes: "))
+
+nota = float(input("Por favor, ingresa tu calificación para ver a qué descuento accedes: "))
 
 if nota >= 6:
     print("Recibirás un descuento del 20%")
-elif    nota >= 5:
+elif nota >= 5:
     print("Recibirás un descuento del 15%")
 elif nota >= 4:
     print("Recibirás un descuento del 10%")
 else:
-    print("Te invitamos a seguir trabajando...")        
+    print("Te invitamos a seguir trabajando...")
