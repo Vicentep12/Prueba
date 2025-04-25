a8d0f25be92dc93ff5cb8d88df97ce8cb37004d4
Vicentep12 <vice.perezg@duocuc.cl>
2025-04-25 19:47:45 -0400

    Add files via upload

---
+++ 23.04Falabella.py
@@ -0,0 +1,10 @@
+print("Bienvenido a Puntos Falabella\nPor favor, ingresa tu nombre y cantidad de puntos para conocer tus descuento: ")
+nombre = input("Nombre: ")
+puntos = int(input("Puntos: "))
+
+if puntos >= 1000:
+    print(f"Genial! {nombre} Estas en el nivel premium, tu descuento será del 25%")
+elif puntos >= 500:
+    print(f"Genial! {nombre} Estas en el nivel oro, tu descuento será del 10%")
+elif puntos >= 250:
+    print(f"Genial {nombre} Estas en el nivel bromce, podrás acceder a diferentes premios")
