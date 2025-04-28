7cef615d13c00d9c02154b95c145dab9ae3345e5
Vicentep12 <vice.perezg@duocuc.cl>
2025-04-28 11:19:36 -0400

    Add files via upload

---
+++ Duda.py
@@ -0,0 +1,26 @@
+import random
+
+print("Bievenido al game de vixo, ingresa 2 numeros")
+
+n1 = int(input("Ingres el primero numero: "))
+n2 = int(input("Ingresa el segundo numero: "))
+
+numero_aleatorio = random.randint(n1, n2)
+
+print (f"El numero es{numero_aleatorio}")
+
+porcentaje = float(input("Ingrese el % que desea: "))
+
+resultado = numero_aleatorio *(porcentaje/100)
+
+print(f"Resultado {resultado}")
+
+print("A continuación, elige si quieres sumar (1) o restar (2)")
+operacion = int(input())
+
+if operacion == 1:
+    numero_final = resultado+numero_aleatorio
+elif operacion == 2:
+    numero_final = resultado-numero_aleatorio
+else:
+    print("Opcion no valdia.")        s
